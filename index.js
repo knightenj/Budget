ccfc8a096028caa69405fc3c205602b8b38bf9a7
knightenj <knightenj@hotmail.com>
2022-09-13 19:26:25 -0700

    more data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let annualExpenses = 0
 
 //Weekly Expense For Loop
 for (let i = 0; i < weeklyExpensesQuestions.length; i++){
-    let stringAnswer = window.prompt(weeklyExpensesQuestions[0]);
+    let stringAnswer = window.prompt(weeklyExpensesQuestions[i]);
     let numberAnswer = parseFloat (stringAnswer)
     weeklyExpenses = weeklyExpenses + numberAnswer;
 }
