b791fbdce221b4e72c857b166dfb8c40c8ead54e
knightenj <knightenj@hotmail.com>
2022-09-13 19:39:48 -0700

    added month and annual

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 //Weekly Expense For Loop
-for (let i = 0; i < weeklyExpensesQuestions.length; i++){
-    let stringAnswer = window.prompt(weeklyExpensesQuestions[i]);
+for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i]);
     let numberAnswer = parseFloat (stringAnswer)
     weeklyExpenses = weeklyExpenses + numberAnswer;
 }
 
+//Monthly Expense For Loop
+for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i]);
+    let numberAnswer = parseFloat (stringAnswer)
+    monthlyExpenses = monthlyExpenses + numberAnswer;
+}
+
+//Annual Expense For Loop
+for (let i = 0; i < annualExpenseQuestions.length; i++){
+    let stringAnswer = window.prompt(annualExpenseQuestions[i]);
+    let numberAnswer = parseFloat (stringAnswer)
+    annualExpenses = annualExpenses + numberAnswer;
+}
 // Your Code Here
