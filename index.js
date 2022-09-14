a9ba008da3277cab2113a46e288a3bcc77c89d93
knightenj <knightenj@hotmail.com>
2022-09-13 19:18:35 -0700

    new data

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+//Weekly Expense For Loop
+for (let i = 0; i < weeklyExpensesQuestions.length; i++){
+    let stringAnswer = window.prompt(weeklyExpensesQuestions[0]);
+    let numberAnswer = parseFloat (stringAnswer)
+    weeklyExpenses = weeklyExpenses + numberAnswer;
+}
+
 // Your Code Here
